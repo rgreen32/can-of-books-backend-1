325266e03a1ca9c92ac1fdc93279feaa530d27f3
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-05-23 18:58:09 -0500

    complete requirements for lab 11 part 2

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 'use strict';
-
+// load environment variables from the .env file
 require('dotenv').config();
+
+// import dependencies
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const app = express();
+
+// bring in the Book model
+const Book = require('./models/Book.js');
+
+// configure express middleware
 app.use(cors());
 
+// configure port
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
 
-  response.send('test request received')
+// book route
+app.get('/books', async (req, res) => {
+  
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    const books = await Book.find({});
+    mongoose.disconnect();
+    res.json(books);
+  } catch (error) {
+    res.json(error.message);
+  }
 
 })
 
